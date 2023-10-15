2fea9fce7e83c70c9ffccce7ee0a5e3fbfe9f27b
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-15 21:49:54 +0200

    ....

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
-// const mailAccessList = ['Vincenzo','Giuseppe','Carlo','Luca'];
 
 // const button = document.getElementById('btnStart');
 
 // button.addEventListener('click',function(){
-//     let mail = document.getElementById('user-mail').value;
-//     console.log(mail)
-// })
+    //     let mail = document.getElementById('user-mail').value;
+    //     console.log(mail)
+    // })
+    
+    
 
-// const msgContainer = document.getElementById('d-mail');
-// msgContainer.innerHTML = `
-// La tua mail:
-//     <p><em>${mailAccessList} </em></p>
-// `;
+const mailAccessList = ['Vincenzo','Giuseppe','Carlo','Luca'];
+let button = document.querySelector('#btnStart')
+button.addEventListener('click', function(){
+    let mail = document.getElementById('user-mail').value;
+    if(mail === 0 || [1]|| [2]||[3]){
+        for(mail = mailAccessList[0]; mail === mailAccessList [0];mail++){
+            const msgContainer = document.getElementById('d-mail');
+            msgContainer.innerHTML = `La tua mail:<p><em>${mail} </em></p>`;
+console.log(mail)
+        }
+    }
+})
+for(let i = [0]; i < mailAccessList.length; i++){
+    mail = mailAccessList[0]||[1]||[2]||[3]
+}
