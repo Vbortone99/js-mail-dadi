61d5d00c65eeee1a06bcbfc50ed9dfbeac13f140
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-15 21:04:22 +0200

    .

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// const mailAccessList = ['Vincenzo','Giuseppe','Carlo','Luca'];
+
+// const button = document.getElementById('btnStart');
+
+// button.addEventListener('click',function(){
+//     let mail = document.getElementById('user-mail').value;
+//     console.log(mail)
+// })
+
+// const msgContainer = document.getElementById('d-mail');
+// msgContainer.innerHTML = `
+// La tua mail:
+//     <p><em>${mailAccessList} </em></p>
+// `;
